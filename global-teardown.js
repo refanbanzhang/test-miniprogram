230e5c50cec99269fe45929daf439838ede75145
refanbanzhang <gaoqiang19514@163.com>
2025-01-05 11:07:43 +0800

    refactor: simplify global teardown logic and update Jest configuration

--- global-teardown.js
+++ global-teardown.js
@@ -1,15 +1,3 @@
-const closeMiniProgram = async () => {
-  try {
-    if (global.__miniProgram__) {
-      await global.__miniProgram__.close()
-      console.log('小程序已关闭')
-    }
-  } catch (error) {
-    console.error('关闭小程序失败:', error)
-    throw error
-  }
-}
-
 module.exports = async () => {
-  await closeMiniProgram()
+  await global.__miniProgram__.close()
 }
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  // maxConcurrency: 1,
   testTimeout: 100 * 1000,
   globalSetup: '<rootDir>/global-setup.js',
   globalTeardown: '<rootDir>/global-teardown.js',
